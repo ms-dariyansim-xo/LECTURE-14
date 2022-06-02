2b96cb70c05d71ff6daee5b21a8eb5cd1d58cfe2
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 18:21:17 -0700

    finalized corrections

--- script/js.js
+++ script/js.js
@@ -1,16 +1,18 @@
 //JavaScript alert worked, so it was commented out.
 //alert();
 
-const display = document.getElementsByClassName("display");
+const display = document.getElementsByClassName("display")[0];
 const userInput = document.getElementsByClassName("input");
-const btnDisplay = document.get
+const btnDisplay = document.getElementById("btnDisplay");
 
 btnDisplay.addEventListener('click', DisplayInfo);
 
-function DisplayInfo(){
+function DisplayInfo()
+{
 
-for(let i = 0; i < userInput.length; i++){
-    display.innerText =+ userInput[i].value + "/n"; 
+for(let i = 0; i < userInput.length; i++)
+{
+    display.innerText += userInput[i].value + "\n"; 
 }
 
 }
