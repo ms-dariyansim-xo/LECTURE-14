9c8354ddda3079799b7e37f6702abd054145c392
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 17:47:34 -0700

    alert added, testing to see if successful

--- script/js.js
+++ script/js.js
@@ -1,2 +1,8 @@
 //JavaScript alert worked, so it was commented out.
 //alert();
+
+const display = document.getElementsByClassName("display")[0];
+const btnDisplay = document.getElementbyId();
+
+alert(display.innerHTML);
+
